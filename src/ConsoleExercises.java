4b97359ccd1d926b739096b84907e560b92aa6b0
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-14 12:00:58 -0500

    created ConsoleExercises file

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f.%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+
+        System.out.println("User input: " + userInput);
+        scanner.nextLine();
+
+        // Prompt and store 3 words
+        System.out.print("Enter the first word: ");
+        String word1 = scanner.nextLine();
+
+        System.out.print("Enter the second word: ");
+        String word2 = scanner.nextLine();
+
+        System.out.print("Enter the third word: ");
+        String word3 = scanner.nextLine();
+
+        System.out.println("You entered:");
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+
+        // Prompt and store a sentence
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.println("Entered sentence: " + sentence);
+
+        // Prompt for length
+        System.out.print("Enter the length of the classroom: ");
+        String lengthInput = scanner.nextLine();
+        double length = Integer.parseInt(lengthInput);
+
+        // Prompt for width
+        System.out.print("Enter the width of the classroom: ");
+        String widthInput = scanner.nextLine();
+        double width = Integer.parseInt(widthInput);
+
+        // Calculate area and perimeter
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+
+
+        System.out.println("Area of the classroom: " + area);
+        System.out.println("Perimeter of the classroom: " + perimeter);
+    }
+}
