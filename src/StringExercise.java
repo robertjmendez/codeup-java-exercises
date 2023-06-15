564544329974e8dad4c347c3ca0e7d57265d29aa
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-15 10:09:13 -0500

    Completed string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,26 @@
+public class StringExercise {
+    public static void main(String[] args) {
+// A. For each of the following output examples: create a String variable that contains the desired output and print it
+// out to the console, you can do this with only one String variable and one print statement for each output example.
+
+// 1. We don't need no education
+// We don't need no thought control
+    String message1 = "We don't need no education\nWe don't need no thought control";
+        System.out.println(message1);
+
+//2 . Check "this" out!, "s inside of "s!
+    String message2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message2);
+
+// 3. In windows, the main drive is usually C:\
+        String message3 = "In windows, the main drive is usually C:\\";
+        System.out.println(message3);
+
+//4. I can do backslashes \, double backslashes \\,
+//  and the amazing triple backslash \\\!
+        String message4 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(message4);
+    }
+}
+
+public
