7ee6c9dd934d2e72692a7984df244403e1429f7d
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-21 12:01:24 -0500

    Grabbed input on same line

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -6,9 +6,8 @@ public class CircleApp {
     public static void main(String[] args) {
         Input input = new Input();
         System.out.print("Enter the radius of the circle: ");
-        double radius = input.getDouble();
 
-        Circle circle = new Circle(radius);
+        Circle circle = new Circle(input.getDouble());
         System.out.printf("Area of the circle: %.2f%n", circle.getArea());
         System.out.printf("Circumference of the circle: %.2f%n", circle.getCircumference());
     }
