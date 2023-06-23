da92c487a8d81c26598eaef09fb6ea7482cabeaa
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-23 12:35:50 -0500

    created the addPerson method

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,4 +1,16 @@
 public class ArraysExercises {
+
+    public static Person[] addPerson(Person[] originalArray, Person personToAdd) {
+        Person[] newArray = new Person[originalArray.length + 1];
+
+        for (int i = 0; i < originalArray.length; i++) {
+            newArray[i] = originalArray[i];
+        }
+
+        newArray[newArray.length - 1] = personToAdd;
+
+        return newArray;
+    }
     public static void main(String[] args) {
     //        int[] numbers = {1, 2, 3, 4, 5};
     //        System.out.println(numbers);
@@ -8,8 +20,17 @@ public class ArraysExercises {
         personArray[1] = new Person("Bob");
         personArray[2] = new Person("Carl");
 
-        for (Person person: personArray) {
+        Person personToAdd = new Person("Alice");
+
+//        for (Person person: personArray) {
+//            System.out.println(person.getName());
+//        }
+
+        Person[] newArray = addPerson(personArray, personToAdd);
+
+        for (Person person: newArray) {
             System.out.println(person.getName());
         }
+
     }
 }
