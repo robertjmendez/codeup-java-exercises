fd3187d0a551ec450cf04e04b4111e26bc9a0810
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-23 12:18:02 -0500

    created an array with person objects

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,5 +1,15 @@
 public class ArraysExercises {
     public static void main(String[] args) {
+    //        int[] numbers = {1, 2, 3, 4, 5};
+    //        System.out.println(numbers);
 
+        Person[] personArray = new Person[3];
+        personArray[0] = new Person("Billy");
+        personArray[1] = new Person("Bob");
+        personArray[2] = new Person("Carl");
+
+        for (Person person: personArray) {
+            System.out.println(person.getName());
+        }
     }
 }
