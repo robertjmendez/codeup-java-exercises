31c2d6f3c52db695588045988a8c6d14832fe6d6
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-24 12:41:03 -0500

    Created ShapesTest class

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,21 @@
 package shapes;
 
 public class Square extends Rectangle{
+    private double side;
     public Square(double side) {
         super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        System.out.println("Square's getArea() method is called.");
+        return Math.pow(side, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        System.out.println("Square's getPerimeter() method is called.");
+        return 4 * side;
     }
 }
