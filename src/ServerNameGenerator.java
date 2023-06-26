ed6756f01888af07530997325c678fac485e4ed3
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-26 11:16:55 -0500

    Added comments to the code

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,24 +1,28 @@
 import java.util.Random;
 
 public class ServerNameGenerator {
-
+    // Create a method that generates a random combination of a noun and an adjective
     public static String generateRandom(String[] noun, String[] adjective) {
+        // Create a random object
         Random random = new Random();
-
+        // Generate a random index for the noun array
         int nounIndex = random.nextInt(noun.length);
+        // Generate a random index for the adjective array
         int adjectiveIndex = random.nextInt(adjective.length);
-
+        // Select a random noun and adjective using the generated indices
         String randomNoun = noun[nounIndex];
         String randomAdjective = adjective[adjectiveIndex];
-
+        // Combine the adjective and noun with a hyphen and return the result
         return randomAdjective + "-" + randomNoun;
     }
 
     public static void main(String[] args) {
+        // Create an array of nouns
         String[] nouns = {"sun", "moon", "flower", "bird", "mountain", "river", "ocean", "tree", "cloud", "star"};
+        // Create an array of adjectives
         String[] adjectives = {"beautiful", "creative", "delightful", "exquisite", "fantastic", "gorgeous", "happy", "incredible", "joyful", "lovely"};
 
-
+        // Generate a random combination of noun and adjective and print it
         System.out.println("Here is your server name:");
         System.out.println(generateRandom(nouns, adjectives));
 
