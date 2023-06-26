cf09a7e5db06e492f057b25df405897b257bb1e2
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-26 15:00:33 -0500

    Added comments to the code

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -3,19 +3,23 @@ package movies;
 import util.Input;
 
 public class MoviesApplication {
-
+    // Method to print out all movies in the array
     private static void viewAllMovies() {
+        // Get all movies
         Movie[] movies = MoviesArray.findAll();
         System.out.println("List of all movies:");
+        // Iterate through each movie and print its name and category
         for (Movie movie : movies) {
             System.out.println(movie.getName() + " -- " + movie.getCategory());
         }
         System.out.println();
     }
-
+    // Method to print out movies from a specific category
     private static void viewMoviesByCategory(String category) {
+        // Get all movies
         Movie[] movies = MoviesArray.findAll();
         System.out.println("Movies in the " + category + " category:");
+        // Iterate through each movie and if it matches the category, print its name
         for (Movie movie : movies) {
             if (movie.getCategory().equalsIgnoreCase(category)) {
                 System.out.println(movie.getName());
@@ -24,10 +28,10 @@ public class MoviesApplication {
         System.out.println();
     }
     public static void main(String[] args) {
+        // Create an instance of the Input class
         Input input = new Input();
-
+        // Print out menu options
         System.out.println("What would you like to do?\n");
-
         System.out.println("0 - exit\n" +
                 "1 - view all movies\n" +
                 "2 - view movies in the animated category\n" +
@@ -36,9 +40,12 @@ public class MoviesApplication {
                 "5 - view movies in the scifi category\n");
 
         int choice;
+        // Loop until the user chooses to exit
         do {
+            // Get user's choice
             choice = input.getInt();
             System.out.println();
+            // Handle user's choice
             switch (choice) {
                 case 0 -> System.out.println("Exiting the application...");
                 case 1 -> viewAllMovies();
