ca30dd9fe2e027b06df6079457dbb7ab4697fd47
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-28 13:01:35 -0500

    tested student class

--- src/grades/StudentTest.java
+++ src/grades/StudentTest.java
@@ -1,4 +1,21 @@
 package grades;
 
 public class StudentTest {
+    public static void main(String[] args) {
+        Student student1 = new Student("Jimbo");
+        student1.addGrade(75);
+        student1.addGrade(60);
+        student1.addGrade(70);
+
+        Student student2 = new Student("Paul");
+        student2.addGrade(70);
+        student2.addGrade(78);
+
+        Student student3 = new Student("Sally");
+        student3.addGrade(60);
+        student3.addGrade(70);
+
+        System.out.println("student1.getGradeAverage() = " + student1.getGradeAverage());
+        System.out.println("student1.getName() = " + student1.getName());
+    }
 }
