68bf9702561655ed85a56a1ef5d3c2ffb9de7505
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-28 12:27:57 -0500

    Setting up student class

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,6 +1,30 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private ArrayList <Integer> grades;
 
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<Integer>();
+    }
 
+    // returns the student's name
+    public String getName() {
+        return name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
 }
